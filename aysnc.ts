3983b3e651e53eae2da4f2617c466c2a5b5290d0
aripineninavya-rgb <aripineninavya@gmail.com>
2026-02-13 17:21:35 +0100

    Updated code according to insdtructions

--- aysnc.ts
+++ aysnc.ts
@@ -1,38 +1,59 @@
 const flipCoin = (): Promise<string> => {
   return new Promise((resolve, reject) => {
-    let result = Math.random();
+    const result = Math.random();
+    result
+      ? resolve("You win! It was heads.")
+      : reject("You lose! It was tails.");
 
-    if (result > 0.5) {
-      resolve("You win! It was heads.");
-    } else {
-      reject("You lose! It was tails.");
-    }
+    // if (result > 0.5) {
+    //   resolve("You win! It was heads.");
+    // } else {
+    //   reject("You lose! It was tails.");
+    // }
   });
 };
 
-const getAdvice = async (): Promise<string> => {
-  const response = await fetch(
-    `https://api.adviceslip.com/advice?timestamp=${Date.now()}`,
-  );
-
-  if (!response.ok) {
-    throw new Error("Failed to fetch advice");
-  }
-
-  const data = await response.json();
-  return data.slip.advice;
-};
-
-const playGame = async () => {
+const coinFlipResult = async () => {
   try {
     const result = await flipCoin();
     console.log(result);
-
-    const advice = await getAdvice();
-    console.log("Your reward advice:", advice);
   } catch (error) {
     console.log(error);
   }
 };
 
-playGame();
+coinFlipResult();
+
+const fetchAdvice = async () => {
+  try {
+    const response = await fetch(
+      `https://api.adviceslip.com/advice?timestamp=${Date.now()}`,
+    );
+    const data = await response.json();
+    console.log(data.slip.advice);
+  } catch (error) {
+    console.error("Error fetching advice:", error);
+  }
+
+  // if (!response.ok) {
+  //   throw new Error("Failed to fetch advice");
+  // }
+};
+
+fetchAdvice();
+
+const getAdviceAfterFlip = async () => {
+  try {
+    const result = await flipCoin();
+    console.log(result);
+    const response = await fetch(
+      `https://api.adviceslip.com/advice?timestamp=${Date.now()}`,
+    );
+    const data = await response.json();
+    console.log(data.slip.advice);
+  } catch (error) {
+    console.error("Error:", error);
+  }
+};
+
+getAdviceAfterFlip();
